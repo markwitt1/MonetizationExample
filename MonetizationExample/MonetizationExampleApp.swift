750ee9d77efd5594fc2cfa4006dccf5a5df34eaa
Mark Witt <mwitt@newstore.com>
2023-12-22 17:13:43 +0100

    Initial Commit

---
+++ MonetizationExample/MonetizationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonetizationExampleApp.swift
+//  MonetizationExample
+//
+//  Created by Mark on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct MonetizationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
